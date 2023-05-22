527bdb91303bc882ba2c8a7cc69a44cfc3506f8b
annmaryvinod <annmarypriyavinod@gmail.com>
2023-05-22 08:53:37 +0530

    created

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let tbody = document.getElementById("tbody")
 
 
 // fetch function
-fetch("http://localhost:3000/user")
+fetch(" http://localhost:3000/user")
     .then(res => res.json())
     .then(json => {
         json.map(data => {
@@ -14,31 +14,24 @@ fetch("http://localhost:3000/user")
     })
 
 // create td
-function td_fun({ profile, name, email, status, role}){
+function td_fun({ image,title,desc}){
     let td = document.createElement('tr');
     td.innerHTML = `
     <td class="px-6 py-4 whitespace-nowrap">
         <div class="flex items-center">
                 <div class="flex-shrink-0 h-10 w-10">
-                    <img src="${profile}" class="h-10 w-10 rounded-full" alt="">
+                    <img src="${image}" class="h-10 w-10 rounded-full" alt="">
                 </div>
-                <div class="ml-4">
-                    <div class="text-sm font-medium text-gray-900">
-                        ${name}
-                    </div>
-                    <div class="text-sm text-gray-500">
-                        ${email}
-                    </div>
-                </div>
+                
             </div>
     </td>
     <td class="px-6 py-4 whitespace-nowrap">
         <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-           ${status}
+           ${title}
         </span>
     </td>
     <td class="px-6 py-4 whitespace-nowrap">
-        <span class="text-sm text-gray-500">${role}</span>
+        <span class="text-sm text-gray-500">${desc}</span>
     </td>
     `;
     return td;
